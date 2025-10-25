28bb9d99d8d4f3983813fe4acdaa2dcd8343470e
kbws13 <hsy040506@163.com>
2025-10-25 20:26:44 +0800

    feat(project): 初始化项目基础结构和配置

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import {createBrowserRouter} from "react-router";
+import {Login} from "@/pages/auth/Login.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: Login
+    }
+]);
+
+export default router;
